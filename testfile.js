f8026a8fa38d6c1d1268014b213b956642a53b2f
Akhilleshgoswami <71202802+Akhilleshgoswami@users.noreply.github.com>
2022-07-16 03:16:42 -0700

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -7,14 +7,13 @@
  const WebSocket = require('ws')
  
  const HTTP_HOST = 'https://api.stage.dydx.exchange'
- const WS_HOST = 'wss://api.dydx.exchange/v3/ws'
+ const WS_HOST = 'wss://api.stage.dydx.exchange/v3/ws'
  
  // NOTE: Set up web3 however your prefer to authenticate to your Ethereum account.
  web3 = new Web3()
- web3.eth.accounts.wallet.add("your privat  key")
- const address =  web3.eth.accounts.wallet[0].address
+ web3.eth.accounts.wallet.add("Private KEY ")
  ;((async () => {
- 
+ const address =  '0xE0E24a32A7e50Ea1c7881c54bfC1934e9b50B520'
    client = new DydxClient(HTTP_HOST, { web3 })
    const apiCreds = await client.onboarding.recoverDefaultApiCredentials(address)
    client.apiKeyCredentials = apiCreds
@@ -22,21 +21,52 @@
   address
   );
    const timestamp = new Date().toISOString()
-   const signature = client.private.sign({
-     requestPath: '/ws/accounts',
-     method: 'GET',
+  //  const signature = client.private.sign({
+  //    requestPath: '/ws/v3/orderbook/:market',
+  //    method: 'GET',
+  //    isoTimestamp: timestamp,
+  //  })
+
+  //    const signature = client.private.sign({
+  //    requestPath: '/ws/accounts',
+  //    method: 'GET',
+  //    isoTimestamp: timestamp,
+  //  })
+   
+  //  console.log('this is api key',apiCreds.apiKey)
+  //  const msg = {
+  //    type: 'subscribe',
+  //    channel: 'v3_accounts',
+  //    accountNumber: '0',
+  //    apiKey: "feac35f2-907d-1a2a-eba8-a54deeddde7a",
+  //    signature,
+  //    timestamp,
+  //    passphrase: 'R9BsxCkYV2H2Z6Za7_ue'
+  //  }
+  //   const msg = {
+  //    type: 'subscribe',
+  //    channel: 'v3_orderbook',
+  //    id:'BTC-USD'
+  //  }
+
+ const signature = client.private.sign({
+     requestPath: '/v3/orders',
+     method: 'POST',
      isoTimestamp: timestamp,
    })
-   const msg = {
-     type: 'subscribe',
-     channel: 'v3_accounts',
-     accountNumber: '0',
-     apiKey: apiCreds.key,
-     signature,
-     timestamp,
-     passphrase: apiCreds.passphrase
-   }
- 
+  const order = await client.private.createOrder(
+    {
+      side: "SELL",
+      type:"LIMIT",
+      timeInForce: "GTT",
+      postOnly: false,
+      size: '100',
+      price: '18000',
+      limitFee: '0.015',
+      expiration: '2022-12-21T21:30:20.200Z',
+    },
+    '1', // required for creating the order signature
+  );
    const ws = new WebSocket(WS_HOST)
  
    ws.on('message', (message) => {
